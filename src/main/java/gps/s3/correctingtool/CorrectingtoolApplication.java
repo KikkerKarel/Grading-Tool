1e066c31b992cd14e1216cf0a62d885a6cf9afd1
spgihuij <servihuijbregts@gmail.com>
2020-09-16 11:08:03 +0200

    Initial commit

---
+++ src/main/java/gps/s3/correctingtool/CorrectingtoolApplication.java
@@ -0,0 +1,13 @@
+package gps.s3.correctingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectingtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrectingtoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gps/s3/correctingtool/CorrectingtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package gps.s3.correctingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectingtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
