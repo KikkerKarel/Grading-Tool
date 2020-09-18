22da2a99d60c56e1e7f13b646ce0d00dd2d8e68d
spgihuij <servihuijbregts@gmail.com>
2020-09-18 15:32:38 +0200

    Application.properties edited

--- src/main/java/gps/s3/correctingtool/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package gps.s3.correctingtool;
-
-import org.springframework.data.repository.CrudRepository;
-import gps.s3.correctingtool.Person;
-
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
