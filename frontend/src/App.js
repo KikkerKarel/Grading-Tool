330ed4752e305649d0f5c9f2f38695777afab3ab
solstice257 <43305197+solstice257@users.noreply.github.com>
2020-09-18 14:22:30 +0200

    Footer and Header components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,9 +20,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hallo
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
