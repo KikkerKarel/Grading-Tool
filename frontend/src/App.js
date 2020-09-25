014139e9e0b64388784a07f078877c3e6058b6e4
solstice257 <43305197+solstice257@users.noreply.github.com>
2020-09-25 14:03:06 +0200

    navbar start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hallo
-        </a>
-      </header>
-      <Footer />
+      <Navbar />
     </div>
   );
 }
