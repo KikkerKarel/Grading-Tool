63b5b14aa58f29f112989d8e03921a277f58b82c
jensevent <j.heesb1@gmail.com>
2020-10-09 15:45:30 +0200

    Multiple choice grading added

--- src/main/java/gps/s3/correctingtool/controller/ExamController.java
+++ src/main/java/gps/s3/correctingtool/controller/ExamController.java
@@ -1,14 +1,15 @@
 package gps.s3.correctingtool.controller;
 
-import gps.s3.correctingtool.exam.Exam;
-import gps.s3.correctingtool.exam.ExamItem;
-import gps.s3.correctingtool.exam.IExamItemRepo;
-import gps.s3.correctingtool.exam.IExamRepo;
+import gps.s3.correctingtool.exam.*;
+import gps.s3.correctingtool.services.GradingTool;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/exams")
@@ -16,27 +17,31 @@ public class ExamController {
 
     private final IExamRepo repo;
     private final IExamItemRepo itemRepo;
+    private final GradingTool gradingTool;
 
-    public ExamController(IExamRepo repo, IExamItemRepo itemRepo) {
+    public ExamController(IExamRepo repo, IExamItemRepo itemRepo, GradingTool gradingTool) {
         this.repo = repo;
         this.itemRepo = itemRepo;
+        this.gradingTool = gradingTool;
     }
 
     @RequestMapping("/find/{id}")
-    public Exam findById(@PathVariable("id") int id)
-    {
+    public Exam findById(@PathVariable("id") int id) {
         return repo.findById(id);
     }
 
     @RequestMapping("/examiner/{id}")
-    public Collection<Exam> byExaminer(@PathVariable("id") int id)
-    {
+    public Collection<Exam> byExaminer(@PathVariable("id") int id) {
         return repo.findAllByExaminerId(id);
     }
 
     @RequestMapping("/question/{id}")
-    public Collection<ExamItem> byQuestion(@PathVariable("id") int id)
-    {
+    public Collection<ExamItem> byQuestion(@PathVariable("id") int id) {
         return itemRepo.findExamItemByQuestionId(id);
+    }
+
+    @RequestMapping("/grade/{id}")
+    public Exam gradeExam(@PathVariable("id") int id) {
+        return gradingTool.gradeMcExam(repo.findById(id));
     }
 }
