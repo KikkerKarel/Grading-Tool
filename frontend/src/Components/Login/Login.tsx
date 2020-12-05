01aa69aec918acca938e3e31b30f415441de8490
Youssef <441585@student.fontys.nl>
2020-12-05 16:18:15 +0100

    Login simple formValidation

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,9 +1,11 @@
 import {Component} from "react";
-import {Form} from "react-bootstrap";
+import {Alert, Form} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import * as React from "react";
 import AuthService from "../../Services/auth.service";
 import {Redirect} from "react-router-dom";
+import "./Login.css"
+import {validateNames, validatePassword} from "./FormValidation";
 
 class Login extends Component {
     state = {
@@ -16,9 +18,28 @@ class Login extends Component {
     {
         event.preventDefault();
         console.log(this.state);
-        AuthService.login(this.state.username, this.state.password).then(data => {
-            this.setState({redirect: true});
-        })
+
+        let errrormessage = document.getElementById("errormessage");
+        let errorbox = document.getElementsByClassName("errorbox")[0];
+
+        if(!validateNames(this.state.username) || !validatePassword(this.state.password))
+        {
+            // @ts-ignore
+            errrormessage.innerText = "Vul een geldige gebruikersnaam en wachtwoord combinatie in.";
+            // @ts-ignore
+            errorbox.style.display = "block";
+        }
+        else{
+            AuthService.login(this.state.username, this.state.password).then(() => {
+                this.setState({redirect: true});
+            },error =>{
+                if(error.response.status === 401)
+                    // @ts-ignore
+                    errrormessage.innerText = "Combinatie komt niet overeen, probeer het opnieuw.";
+                    // @ts-ignore
+                    errorbox.style.display = "block";
+            })
+        }
     }
 
     render() {
@@ -27,21 +48,25 @@ class Login extends Component {
 
         return (
             <>
-                <div className="py-5">
-                </div>
-
-                <h1>Login</h1>
-
-                <div className=" mt-5">
+                <div className="mt-5 col-md-6">
                     <Form>
+                        <h1>Login</h1>
+
+                        <Form.Group>
+                            <Alert variant="danger errorbox">
+                                <Alert.Heading>Foutmelding:</Alert.Heading>
+                                <Form.Text id="errormessage"/>
+                            </Alert>
+                        </Form.Group>
+
                         <Form.Group controlId="formBasicName">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control onChange={(event) => { this.setState({username: event.target.value})}} type="username" placeholder="Username" />
+                            <Form.Label>Gebruikersnaam</Form.Label>
+                            <Form.Control onChange={(event) => { this.setState({username: event.target.value})}} type="username" placeholder="Gebruikersnaam" />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control onChange={(event) => { this.setState({password: event.target.value})}} type="password" placeholder="Password" />
+                            <Form.Label>Wachtwoord</Form.Label>
+                            <Form.Control onChange={(event) => { this.setState({password: event.target.value})}} type="password" placeholder="Wachtwoord" />
                         </Form.Group>
                         <Form.Group controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Remember me" />
