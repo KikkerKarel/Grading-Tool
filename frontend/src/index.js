670a1d81ef45a0c32a35db650cb2b390e573e92a
Rachid Boukaiba <rachidd@protonmail.com>
2021-01-10 22:49:43 +0100

    fix attempt env variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
-axios.defaults.baseURL = "http://gradest-api.v99.nl/";
+axios.defaults.baseURL = process.env.REACT_APP_AXIOS_BASE_URL;
 axios.interceptors.request.use(function (config) {
     config.headers.Authorization =  localStorage.getItem("token");
 
